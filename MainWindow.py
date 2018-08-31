5b550ae7912226987a8cf82a0a9b7c8c144661c4
Insane Shadow <insaneshadowzaman@gmail.com>
2018-08-31 23:30:34 +0600

    Added the project

---
+++ MainWindow.py
@@ -0,0 +1,228 @@
+from enum import IntEnum, auto
+
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.uic import loadUi
+
+from WheelChair import WheelChair
+from inputs.Controller import Controller
+
+
+# from testing.Controller import Controller
+
+
+class MODE(IntEnum):
+    CHAIR = auto()
+    MAIN = auto()
+    SUBPROC = auto()
+    AUDIO = auto()
+    VIDEO = auto()
+    BROWSER = auto()
+
+
+class METHOD(IntEnum):
+    EYE_HELP = 0
+    HEAD_HELP = 1
+    VOICE_HELP = 2
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        WINDOW_TITLE = "Eye Based Wheelchair Control & Task Manager"
+        GUI_UI_LOCATION = "./UI/MainWindow.ui"
+
+        super(MainWindow, self).__init__()
+        loadUi(GUI_UI_LOCATION, self)
+        self.setWindowTitle(WINDOW_TITLE)
+        self.main_image_label.setScaledContents(True)
+
+        self.resetButton.clicked.connect(self.resetAll)
+
+        self.chair = WheelChair()
+
+        self.current_mode = MODE.MAIN
+        self.current_focus = 0
+
+        self.__initialize_buttons()
+
+        self.main_controller = Controller(self, self.gotInput)
+
+        self.player = None
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.main_controller.getInput)
+        self.timer.start(10)
+
+    def gotInput(self, command):
+        print("Got input : " + str(command))
+
+        if self.current_mode == MODE.MAIN:
+            if command == "left":
+                self.moveFocusLeft()
+            elif command == "right":
+                self.moveFocusRight()
+            elif command == "up":
+                self.moveFocusUp()
+            elif command == "down":
+                self.moveFocusDown()
+            elif command == "press":
+                self.pressFocused()
+
+        elif self.current_mode == MODE.CHAIR:
+            if command == "left":
+                self.chair.left()
+            elif command == "right":
+                self.chair.right()
+            elif command == "press":
+                self.chair.toggleStartStop()
+            elif command == "exit":
+                self.chair.active = False
+                self.current_mode = MODE.MAIN
+
+        elif self.current_mode == MODE.AUDIO or self.current_mode == MODE.VIDEO:
+            if command == "right":
+                self.player.nextItem()
+            elif command == "left":
+                self.player.destroy()
+                self.current_mode = MODE.MAIN
+            elif command == "press":
+                self.player.play()
+
+
+    def closeEvent(self, event):
+        # self.main_controller.closed()
+        # self.deleteLater()
+        pass
+
+    def resetAll(self):
+        pass
+
+    def __initialize_buttons(self):
+        self.selectMethodComboBox.clear()
+        self.selectMethodComboBox.addItems([
+            "Eye-Help",
+            "Head-Help",
+            "Voice-Help"
+        ])
+        self.selectMethodComboBox.setCurrentIndex(METHOD.EYE_HELP)
+        self.selectMethodComboBox.currentIndexChanged.connect(self.comboboxIndexChanged)
+
+        self.buttons = [self.b1_1, self.b1_2,
+                        self.b1_3, self.b2_1,
+                        self.b2_2, self.b2_3,
+                        self.b3_1, self.b3_2]
+        for b in self.buttons:
+            b.setAutoDefault(True)
+        self.buttons[self.current_focus].setFocus(True)
+
+        self.b1_1.clicked.connect(self.controlWheel)
+        self.b1_2.clicked.connect(self.playSMS)
+        self.b1_3.clicked.connect(self.playEmail)
+        self.b2_1.clicked.connect(self.playVideo)
+        self.b2_2.clicked.connect(self.playMusic)
+        self.b2_3.clicked.connect(self.playBrowser)
+        self.b3_1.clicked.connect(self.playLight)
+        self.b3_2.clicked.connect(self.playFan)
+
+    def comboboxIndexChanged(self):
+        import cv2
+        cv2.destroyAllWindows()
+        # TODO
+        if self.selectMethodComboBox.currentIndex() == METHOD.EYE_HELP:
+            pass
+        elif self.selectMethodComboBox.currentIndex() == METHOD.HEAD_HELP:
+            pass
+        elif self.selectMethodComboBox.currentIndex() == METHOD.VOICE_HELP:
+            pass
+
+    def controlWheel(self):
+        self.current_mode = MODE.CHAIR
+        self.chair.active = True
+        from PyQt5 import QtWidgets
+        sizeObject = QtWidgets.QDesktopWidget().screenGeometry(-1)
+        height = sizeObject.height()
+        width = sizeObject.width()
+        self.move(int(width * 0.5), int(height * 0.1))
+
+    def playFan(self):
+        self.chair.toggleFan()
+
+    def playLight(self):
+        self.chair.toggleLight()
+
+    def playMusic(self):
+        from Players.Audio import Audio
+        self.current_mode = MODE.AUDIO
+        self.player = Audio()
+
+    def playVideo(self):
+        self.current_mode = MODE.VIDEO
+
+    def moveFocusRight(self):
+        if self.current_mode is MODE.MAIN:
+            self.current_focus = (self.current_focus + 1) % 8
+            self.buttons[self.current_focus].setFocus(True)
+
+    def moveFocusLeft(self):
+        self.current_focus = (self.current_focus - 1) % 8
+        self.buttons[self.current_focus].setFocus(True)
+
+    def moveFocusUp(self):
+        self.current_focus = (self.current_focus - 2) % 8
+        self.buttons[self.current_focus].setFocus(True)
+
+    def moveFocusDown(self):
+        self.current_focus = (self.current_focus + 2) % 8
+        self.buttons[self.current_focus].setFocus(True)
+
+    def pressFocused(self):
+        self.buttons[self.current_focus].animateClick()
+
+    def playSMS(self):
+        from zeep import Client
+        try:
+            url = 'https://api2.onnorokomsms.com/sendsms.asmx?WSDL'
+            client = Client(url)
+            userName = '01521313223'
+            password = '90053'
+            recipientNumber = '01521323429'
+            smsText = 'Help Me'
+            smsType = 'TEXT'
+            maskName = ''
+            campaignName = ''
+            client.service.OneToOne(userName, password, recipientNumber, smsText, smsType, maskName, campaignName)
+            print('SMS sent!')
+        except Exception as e:
+            print('SMS nor sent!')
+            print(e)
+
+    def playEmail(self):
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.text import MIMEText
+        import smtplib
+        try:
+            fromaddr = 'eyegaze.kuet@gmail.com'
+            toaddr = 'sakibreza1@gmail.com'
+            msg = MIMEMultipart()
+            msg['From'] = fromaddr
+            msg['To'] = toaddr
+            msg['Subject'] = 'Doctor Appointment'
+
+            body = 'I am facing problem.Please come to see me if you are free.'
+            msg.attach(MIMEText(body, 'plain'))
+
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(fromaddr, '060701cse')
+            text = msg.as_string()
+            server.sendmail(fromaddr, toaddr, text)
+            server.quit()
+            print('Email Sent Successfully')
+        except Exception as e:
+            print('Email not sent')
+            print(e)
+
+    def playBrowser(self):
+        # TODO
+        import webbrowser
+        webbrowser.open_new_tab("http://epaperna.prothomalo.com/")
---
+++ inputs/Controller.py
@@ -0,0 +1,184 @@
+import cv2
+from PyQt5.QtGui import QImage, QPixmap
+
+import MainWindow
+from inputs.BlinkDetector import BlinkDetector
+from inputs.FaceDetector import FaceDetector
+from inputs.GazeDetector import GazeDetector
+from inputs.Speech import Speech
+
+
+# Commands :
+# left
+# right
+# up
+# down
+# press
+# exit
+
+class Controller:
+    def __init__(self, main_window, giveOutput):
+        self.blink_detector = BlinkDetector()
+        self.face_detector = FaceDetector()
+        self.gaze_detector = GazeDetector()
+        # self.speech_detector = Speech()
+        self.giveOutput = giveOutput
+        self.main_window = main_window
+        self.cap = cv2.VideoCapture(0)
+
+    def getInput(self):
+
+        if self.main_window.current_mode is MainWindow.MODE.MAIN or self.main_window.current_mode is MainWindow.MODE.VIDEO or self.main_window.current_mode is MainWindow.MODE.AUDIO:
+            if self.main_window.selectMethodComboBox.currentIndex() == MainWindow.METHOD.EYE_HELP:
+                _, img = self.cap.read()
+
+                dicBlink = self.blink_detector.processImage(img, self.main_window.eyeThreshold.value())
+
+                outImage = toQImage(dicBlink["img"])
+                outImage = outImage.rgbSwapped()
+                self.main_window.main_image_label.setPixmap(QPixmap.fromImage(outImage))
+                label = "Blink Detector : " \
+                        "both : " + str(dicBlink["both"]) + "\n" + \
+                        "left : " + str(dicBlink["left"]) + "\n" + \
+                        "right : " + str(dicBlink["right"]) + "\n" + \
+                        "leftEAR : " + str(dicBlink["leftEAR"]) + "\n" + \
+                        "rightEAR :" + str(dicBlink["rightEAR"]) + "\n" + \
+                        "bothTotal : " + str(dicBlink["bothTotal"]) + "\n" + \
+                        "leftTotal : " + str(dicBlink["leftTotal"]) + "\n" + \
+                        "rightTotal : " + str(dicBlink["rightTotal"]) + "\n"
+                self.main_window.image_info_textlabel.setText(label)
+
+                if dicBlink["left"]:
+                    self.giveOutput("left")
+                    return
+                elif dicBlink["right"]:
+                    self.giveOutput("right")
+                    return
+                elif dicBlink["both"]:
+                    self.giveOutput("press")
+                    return
+
+            elif self.main_window.selectMethodComboBox.currentIndex() == MainWindow.METHOD.HEAD_HELP:
+                _, img = self.cap.read()
+
+                dicBlink = self.blink_detector.processImage(img, self.main_window.eyeThreshold.value())
+
+                outImage = toQImage(dicBlink["img"])
+                outImage = outImage.rgbSwapped()
+                self.main_window.main_image_label.setPixmap(QPixmap.fromImage(outImage))
+                dicHead = self.face_detector.processImage(img)
+
+                label = "Blink Detector : " \
+                        "both : " + str(dicBlink["both"]) + "\n" \
+                        "left : " + str(dicBlink["left"]) + "\n" \
+                        "right : " + str(dicBlink["right"]) + "\n" \
+                        "leftEAR : " + str(dicBlink["leftEAR"]) + "\n" \
+                        "rightEAR :" + str(dicBlink["rightEAR"]) + "\n" \
+                        "bothTotal : " + str(dicBlink["bothTotal"]) + "\n" \
+                        "leftTotal : " + str(dicBlink["leftTotal"]) + "\n" \
+                        "rightTotal : " + str(dicBlink["rightTotal"]) + "\n" \
+                        "Head Detector : " + "\n" \
+                        "direction : " + str(dicHead["direction"]) + "\n"
+                self.main_window.image_info_textlabel.setText(label)
+
+                head = dicHead["direction"]
+                if head == "left":
+                    self.giveOutput("left")
+                    return
+                elif head == "right":
+                    self.giveOutput("right")
+                    return
+                elif head == "up":
+                    self.giveOutput("up")
+                    return
+                elif head == "down":
+                    self.giveOutput("down")
+                    return
+                elif dicBlink["both"]:
+                    self.giveOutput("press")
+                    return
+                elif dicBlink["right"]:
+                    self.face_detector.initPos(dicHead["face"])
+                    return
+
+        elif self.main_window.current_mode is MainWindow.MODE.CHAIR:
+            if self.main_window.selectMethodComboBox.currentIndex() == MainWindow.METHOD.EYE_HELP:
+                _, img = self.cap.read()
+                dicGaze = self.gaze_detector.processImage(img)
+                dicBlink = self.blink_detector.processImage(img, self.main_window.eyeThreshold.value())
+                label = "Blink Detector : " \
+                        "both : " + str(dicBlink["both"]) + "\n" \
+                        "left : " + str(dicBlink["left"]) + "\n" \
+                        "right : " + str(dicBlink["right"]) + "\n" \
+                        "leftEAR : " + str(dicBlink["leftEAR"]) + "\n" \
+                        "rightEAR :" + str(dicBlink["rightEAR"]) + "\n" \
+                        "bothTotal : " + str(dicBlink["bothTotal"]) + "\n" \
+                        "leftTotal : " + str(dicBlink["leftTotal"]) + "\n" \
+                        "rightTotal : " + str(dicBlink["rightTotal"]) + "\n" \
+                        "Gaze Detector : " + "\n" \
+                        "direction : " + str(dicGaze["direction"]) + "\n"
+                self.main_window.image_info_textlabel.setText(label)
+                outImage = toQImage(dicBlink["img"])
+                outImage = outImage.rgbSwapped()
+                self.main_window.main_image_label.setPixmap(QPixmap.fromImage(outImage))
+                self.giveOutput(dicGaze["direction"])
+                if dicBlink["right"] or dicBlink["left"]:
+                    self.giveOutput("exit")
+                    cv2.destroyAllWindows()
+
+                return
+
+            elif self.main_window.selectMethodComboBox.currentIndex() == MainWindow.METHOD.HEAD_HELP:
+                _, img = self.cap.read()
+                dicBlink = self.blink_detector.processImage(img, self.main_window.eyeThreshold.value())
+                dicHead = self.face_detector.processImage(img)
+
+                label = "Blink Detector : " \
+                        "both : " + str(dicBlink["both"]) + "\n" \
+                        "left : " + str(dicBlink["left"]) + "\n" \
+                        "right : " + str(dicBlink["right"]) + "\n" \
+                        "leftEAR : " + str(dicBlink["leftEAR"]) + "\n" \
+                        "rightEAR :" + str(dicBlink["rightEAR"]) + "\n" \
+                        "bothTotal : " + str(dicBlink["bothTotal"]) + "\n" \
+                        "leftTotal : " + str(dicBlink["leftTotal"]) + "\n" \
+                        "rightTotal : " + str(dicBlink["rightTotal"]) + "\n" \
+                        "Head Detector : " + "\n" \
+                        "direction : " + str(dicBlink["rightTotal"]) + "\n"
+                self.main_window.image_info_textlabel.setText(label)
+
+                outImage = toQImage(dicBlink["img"])
+                outImage = outImage.rgbSwapped()
+                self.main_window.main_image_label.setPixmap(QPixmap.fromImage(outImage))
+                head = dicHead["direction"]
+                if head == "left":
+                    self.giveOutput("left")
+                    return
+                elif head == "right":
+                    self.giveOutput("right")
+                    return
+                elif head == "up":
+                    self.giveOutput("up")
+                    return
+                elif head == "down":
+                    self.giveOutput("down")
+                    return
+                elif dicBlink["both"]:
+                    self.giveOutput("press")
+                    return
+                elif dicBlink["right"]:
+                    self.face_detector.initPos(dicHead["face"])
+                    return
+
+
+def toQImage(raw_img):
+    from numpy import copy
+    img = copy(raw_img)
+    qformat = QImage.Format_Indexed8
+    if len(img.shape) == 3:
+        if img.shape[2] == 4:
+            qformat = QImage.Format_RGBA8888
+        else:
+            qformat = QImage.Format_RGB888
+
+    outImg = QImage(img.tobytes(), img.shape[1], img.shape[0], img.strides[0], qformat)
+    return outImg
